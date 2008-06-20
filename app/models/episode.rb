bf761bbc356d7cc0f1a6cca8b72c320b61ed2aef
Ryan Bates <ryan@railscasts.com>
2008-06-19 19:03:31 -0700

    adding published named scope to episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,6 @@
 class Episode < ActiveRecord::Base
   has_many :taggings
-  has_many :tags, :througth => :taggings
+  has_many :tags, :through => :taggings
+  
+  named_scope :published, lambda { {:conditions => ['published_at <= ?', Time.now]} }
 end
--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -1,11 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Episode do
-  before(:each) do
-    @episode = Episode.new
-  end
-
-  it "should be valid" do
-    @episode.should be_valid
+  it "should find published" do
+    a = Episode.create!(:published_at => 2.weeks.ago)
+    b = Episode.create!(:published_at => 2.weeks.from_now)
+    Episode.published.should include(a)
+    Episode.published.should_not include(b)
   end
 end
