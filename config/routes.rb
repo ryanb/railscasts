d6d691301880ad3027a57708f31e571fe9c7b9d5
Ryan Bates <ryan@railscasts.com>
2008-06-28 16:37:46 -0700

    adding side bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.about 'about', :controller => 'info', :action => 'about'
+  
   map.resources :comments
   map.resources :tags
   map.resources :episodes, :collection => { :recent => :get }
+  
   map.root :recent_episodes
 end
