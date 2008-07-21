1f26033a647a81a7e466dbbf34a774e96f0b24b0
Ryan Bates <ryan@railscasts.com>
2008-07-20 19:40:12 -0700

    adding duration calculation to download model

--- app/models/download.rb
+++ app/models/download.rb
@@ -1,3 +1,8 @@
 class Download < ActiveRecord::Base
   belongs_to :episode
+  
+  def duration
+    min, sec = *seconds.divmod(60)
+    [min, sec.to_s.rjust(2, '0')].join(':')
+  end
 end
--- spec/models/download_spec.rb
+++ spec/models/download_spec.rb
@@ -1,4 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Download do
+  it "should translate single digit seconds into duration with minutes" do
+    Download.new(:seconds => 60*8+3).duration.should == '8:03'
+  end
+  
+  it "should translate double digit seconds into duration with minutes" do
+    Download.new(:seconds => 60*8+12).duration.should == '8:12'
+  end
 end
