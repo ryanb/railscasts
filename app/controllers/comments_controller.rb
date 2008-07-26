390ffbc69b7432f5a7b85a8b48745dc80354aed5
Ryan Bates <ryan@railscasts.com>
2008-07-25 22:23:27 -0700

    limiting comments index list to only 30 comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_filter :authorize, :only => [:edit, :update, :destroy]
   
   def index
-    @comments = Comment.find(:all)
+    @comments = Comment.find(:all, :limit => 30)
   end
   
   def new
