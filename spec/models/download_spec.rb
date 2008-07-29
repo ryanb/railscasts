386ca3e1be17e143d4f8b8dcc24d7a790f2c3e71
Ryan Bates <ryan@railscasts.com>
2008-07-28 18:58:07 -0700

    improving duration handling for episode downloads

--- spec/models/download_spec.rb
+++ spec/models/download_spec.rb
@@ -8,4 +8,12 @@ describe Download do
   it "should translate double digit seconds into duration with minutes" do
     Download.new(:seconds => 60*8+12).duration.should == '8:12'
   end
+  
+  it "should return nil for duration if seconds aren't set" do
+    Download.new(:seconds => nil).duration.should be_nil
+  end
+  
+  it "should parse duration into seconds" do
+    Download.new(:duration => '10:03').seconds.should == 603
+  end
 end
