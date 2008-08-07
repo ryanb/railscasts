c48a97bba6670cea9333c54be3a5ed4b733722de
Ryan Bates <ryan@railscasts.com>
2008-08-07 08:48:49 -0700

    moving session key into external config as well

--- config/environment.rb
+++ config/environment.rb
@@ -52,7 +52,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_railscasts_session',
+    :session_key => APP_CONFIG[:session_key],
     :secret      => APP_CONFIG[:session_secret]
   }
 
