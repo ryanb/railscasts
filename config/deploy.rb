03fc249d276d157350bb8c0893a0361f0657db70
Ryan Bates <ryan@railscasts.com>
2008-08-18 22:34:22 -0700

    symlink sphinx config file on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ namespace :deploy do
   task :symlink_extras do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/app_config.yml #{release_path}/config/app_config.yml"
+    run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
     run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
   end
 
