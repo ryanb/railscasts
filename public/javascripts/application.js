6e15b685b67f4f1613f2a84b4eb580cde5b04076
Ryan Bates <ryan@railscasts.com>
2009-03-14 09:49:50 -0700

    removing unused javascript in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,14 +2,6 @@ jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
-
 $(document).ready(function() {
   $("#comments .spam_report").removeAttr("onclick").click(function () {
     $.post(this.href, null, null, "script");
