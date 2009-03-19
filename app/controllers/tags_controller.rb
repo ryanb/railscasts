971a531374ea59c936eb5349bfbc7179f8adb87b
Ryan Bates <ryan@railscasts.com>
2009-03-19 08:22:34 -0700

    fixing error caused by count when there were more than ten episodes in a tag show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
-    @episodes = @tag.episodes.published.recent.paginate(:page => params[:page], :per_page => 10)
+    @episodes = @tag.episodes.published.recent.paginate(:page => params[:page], :per_page => 10, :count => { :select => "*" })
   end
 end
