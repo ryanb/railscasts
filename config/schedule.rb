3472d9a7697050b28939514a1fea8d6b464a656f
Ryan Bates <ryan@railscasts.com>
2009-07-22 11:55:19 -0700

    automatically trigger rake asciicasts once each day

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ every 1.day, :at => "2:34 am" do
   rake "thinking_sphinx:index"
 end
 
+every 1.day, :at => "1:15 am" do
+  rake "asciicasts"
+end
+
 every :reboot do
   rake "thinking_sphinx:start"
 end
