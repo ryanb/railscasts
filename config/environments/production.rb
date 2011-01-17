d124319f4ca2a2367c1fa705f5c8229cce70921d
Ryan Bates <ryan@railscasts.com>
2011-01-17 09:37:04 -0800

    changing production log level to warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Railscasts::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
