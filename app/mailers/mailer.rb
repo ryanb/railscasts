84051ef0957a7debbe7aeb689f16651e3626fa82
Ryan Bates <ryan@railscasts.com>
2011-05-11 09:20:55 -0700

    include RailsCasts in feedback email subject

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,6 @@
 class Mailer < ActionMailer::Base
   def feedback(message)
     @message = message
-    mail :to => "feedback@railscasts.com", :from => @message.email, :subject => "Feedback from #{@message.name}"
+    mail :to => "feedback@railscasts.com", :from => @message.email, :subject => "RailsCasts Feedback from #{@message.name}"
   end
 end
