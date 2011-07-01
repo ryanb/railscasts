e05390372bf2d48b81e3a845e71116c91a8121a9
Ryan Bates <ryan@railscasts.com>
2011-06-30 21:07:21 -0700

    adding moderator info page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Railscasts::Application.routes.draw do
   match "auth/:provider/callback" => "users#create"
   match "about" => "info#about", :as => "about"
   match "give_back" => "info#give_back", :as => "give_back"
+  match "moderators" => "info#moderators", :as => "moderators"
   match "login" => "users#login", :as => "login"
   match "logout" => "users#logout", :as => "logout"
   match "feedback" => "feedback_messages#new", :as => "feedback"
